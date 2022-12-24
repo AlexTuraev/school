09d697b084f1980666d5ee48dbed63244364fc8c
Alex Turaev <altex_76@mail.ru>
2022-12-24 15:49:21 +0200

    hw4.4

--- src/main/java/ru/hogwarts/school/service/FacultyService.java
+++ src/main/java/ru/hogwarts/school/service/FacultyService.java
@@ -1,5 +1,7 @@
 package ru.hogwarts.school.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ru.hogwarts.school.model.Faculty;
@@ -7,11 +9,11 @@ import ru.hogwarts.school.model.Student;
 import ru.hogwarts.school.repository.FacultyRepository;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
 @Service
 public class FacultyService{
 
+    Logger logger = LoggerFactory.getLogger(FacultyService.class);
     //@Autowired
     private final FacultyRepository facultyRepository;
 
@@ -20,16 +22,19 @@ public class FacultyService{
     }
 
     public Faculty createFaculty(Faculty faculty) {
+        logger.info("The method {} was called", "<<createFaculty>>");
         return facultyRepository.save(faculty);
     }
 
     public Faculty getFacultyById(Long id) {
+        logger.info("The method {} was called", "<<getFacultyById>>");
         Faculty f = facultyRepository.findById(id).orElse(null);
         //return facultyRepository.findById(id).orElse(null);
         return f;
     }
 
     public Faculty updateFaculty(Faculty faculty) {
+        logger.info("The method {} was called", "<<updateFaculty>>");
         if (getFacultyById(faculty.getId()) == null) {
             return null;
         }else {
@@ -38,6 +43,7 @@ public class FacultyService{
     }
 
     public Faculty deleteFacultyById(Long id) {
+        logger.info("The method {} was called", "<<deleteFacultyById>>");
         Faculty faculty = getFacultyById(id);
         if (faculty == null) {
             return null;
@@ -48,13 +54,16 @@ public class FacultyService{
     }
 
     public List<Faculty> findByColor(String color) {
+        logger.info("The method {} was called", "<<findByColor>>");
         return facultyRepository.findByColor(color);
     }
     public Collection<Faculty> getAllFaculties() {
+        logger.info("The method {} was called", "<<getAllFaculties>>");
         return facultyRepository.findAll();
     }
 
     public List<Faculty> getFacultiesByColorOrName(String param) {
+        logger.info("The method {} was called", "<<getFacultiesByColorOrName>>");
         List <Faculty> facultyList = facultyRepository.findByColorIgnoreCase(param);
         facultyList.addAll(facultyRepository.findByNameIgnoreCase(param));
 
@@ -63,6 +72,7 @@ public class FacultyService{
     }
 
     public List<Student> getStudentsByIdOfFaculty(Long id) {
+        logger.info("The method {} was called", "<<getStudentsByIdOfFaculty>>");
         Faculty faculty = facultyRepository.findById(id).orElse(null);
 
         if (faculty == null) {
